ac18ebf091430a81391f868a28e431f53b3367cc
Pan Ziyue <random.rrr3@gmail.com>
2014-08-10 16:55:28 +0800

    Initial Commit

---
+++ iBeaconDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iBeaconDemo
+//
+//  Created by Pan Ziyue on 10/8/14.
+//  Copyright (c) 2014 StatiX Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ iBeaconDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iBeaconDemo
+//
+//  Created by Pan Ziyue on 10/8/14.
+//  Copyright (c) 2014 StatiX Industries. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
